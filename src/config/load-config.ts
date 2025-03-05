19eaa7779f0b8e8cd14607c1859000314719de33
KietCSE <ptkiet170104@gmail.com>
2025-03-05 23:08:32 +0700

    feat: add sample adafruit

--- src/config/load-config.ts
+++ src/config/load-config.ts
@@ -3,7 +3,9 @@ dotenv.config()
 
 const config = {
     PORT: Number(process.env.PORT || 3000),
-
+    AIO_USERNAME: String(process.env.AIO_USERNAME),
+    AIO_KEY: String(process.env.AIO_KEY),
+    AIO_FEED_ID: String(process.env.AIO_FEED_ID)
 };
 
 export default config;
