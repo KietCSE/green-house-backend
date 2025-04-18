9b3eae943ce0c92226d0349c33663248c9b81e66
KietCSE <ptkiet170104@gmail.com>
2025-04-18 20:37:02 +0700

    fix: docs of user notification

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -14,7 +14,9 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: `https://api-smartgarden.meowstation.me/api`, // URL của server
+                // url: `https://api-smartgarden.meowstation.me/api`, // URL của server
+                url: `http://localhost:3000/api`, // URL của server
+
             },
         ],
     },
